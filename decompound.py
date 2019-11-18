e1c8201e3fbaac0f951d659eccc6b2d9fe20f4e0
Bruno BELANYI <bruno.belanyi@epita.fr>
2019-11-18 13:31:17 +0100

    Update: factorize compounding into single Class
    The only thing left to do is to factorize the actual decompounding use
    of the class in each script into a single method which can do the
    expected behaviour.

---
+++ decompound.py
@@ -0,0 +1,262 @@
+import gzip
+import logging
+from dataclasses import dataclass, field
+from enum import IntEnum
+from typing import IO, Dict, Iterable, List, Optional, Set, Tuple
+
+
+def nopen(f: str) -> IO[str]:
+    if f.endswith(".gz"):
+        return gzip.open(f)
+    return open(f, encoding="utf-8")
+
+
+def contained_in(c: str, cands: Iterable[str]) -> bool:
+    for cj in cands:
+        if c.lower() in cj.lower() and c.lower() != cj.lower():
+            return True
+    return False
+
+
+def get_max_idx(ls: Iterable[float]) -> Tuple[int, float]:
+    """
+    Return the index and value of the maximum in the given iterable,
+    returns (-1, 0.0) on empty iterables
+    """
+    return max(enumerate(ls), key=lambda x: x[1], default=(-1, 0.0))
+
+
+@dataclass
+class Decompounder:
+    class DashBehaviour(IntEnum):
+        """
+        Which heuristic should the decompounder use to split dashed-words.
+        """
+
+        REMOVE = 1
+        SPLIT = 2
+        IGNORE = 3
+
+    epsilon: float
+    min_word_length: int = 5
+    min_word_count: int = 50
+    prefix_length: int = 3
+    suffix_length: int = 3
+    dash_words: DashBehaviour = DashBehaviour.IGNORE
+    uppercase_first_letter: bool = False
+    single_words: Set[str] = field(default_factory=set, init=False)
+    # count suffixes and prefixes
+    fillers: Dict[str, int] = field(default_factory=dict, init=False)
+    total_word_count: int = field(default=0, init=False)
+    word_count: Dict[str, int] = field(default_factory=dict, init=False)
+    comp1: Dict[str, str] = field(default_factory=dict, init=False)
+    comp2: Dict[str, str] = field(default_factory=dict, init=False)
+    comp3: Dict[str, str] = field(default_factory=dict, init=False)
+
+    def _remove_word(self, w: str) -> bool:
+        if len(w.replace("-", "")) == 0:
+            return True
+        if self.min_word_count <= 0:
+            return False
+        return self.word_count.get(w, 0) < self.min_word_count
+
+    def _remove_short_and_equal(self, wc: str, ws: Iterable[str]) -> List[str]:
+        nws = set()
+        for w in ws:
+            if (
+                len(w) >= self.min_word_length
+                and w.lower() != wc.lower()
+                and not w.isupper()
+                and w.lower() in wc.lower()
+            ):
+                nws.add(w)
+        return list(nws)
+
+    def _add_up(self, w: str) -> None:
+        if w in self.fillers:
+            self.fillers[w] += 1
+        else:
+            self.fillers[w] = 1
+
+    def _append_suffix(self, w: str) -> str:
+        nl = ""
+        # first append on the left side
+        for l in w.split("-"):
+            if len(l) > self.suffix_length:
+                nl += "-"
+            else:
+                self._add_up(l)
+            nl += l
+        nl = nl.strip("-")
+        return nl
+
+    def _append_prefix(self, w: str) -> str:
+        # append to the right
+        nl = ""
+        for l in w.split("-"):
+            nl += l
+            if len(l) > self.prefix_length:
+                nl += "-"
+        if nl.endswith("-"):
+            nl = nl[:-1]
+        return nl
+
+    def _get_word_counts(self, comp: str) -> float:
+        sum = 1.0
+        for c in comp.split("-"):
+            if self.uppercase_first_letter:
+                c = c[0].upper() + c[1:]
+            if c in self.word_count:
+                sum *= (self.word_count[c] + self.epsilon) / (
+                    self.total_word_count + self.epsilon * len(self.word_count)
+                )
+            else:
+                sum *= self.epsilon / (
+                    self.total_word_count + self.epsilon * len(self.word_count)
+                )
+        return pow(1.0 * sum, 1.0 / len(comp.split("-")))
+
+    def _append_suffix_and_prefix(self, w: str) -> str:
+        sp = self._append_suffix(self._append_prefix(w))
+        ps = self._append_prefix(self._append_suffix(w))
+        spc = self._get_word_counts(sp)
+        psc = self._get_word_counts(ps)
+        if spc > psc:
+            return sp
+        return ps
+
+    def _generate_compound(self, w: str, ws: Iterable[str]) -> Optional[str]:
+        # remove too short words
+        nws = self._remove_short_and_equal(w, ws)
+        if len(nws) == 0:
+            logging.debug(f"NONE: {w}")
+            return None
+        nws_sorted = sorted(nws, key=lambda x: len(x), reverse=True)
+        # get split points
+        splits = set()
+        for n in nws_sorted:
+            if not n.lower() in w.lower():
+                continue
+            idx = w.lower().index(n.lower())
+            splits.add(idx)
+            splits.add(idx + len(n))
+        splits_sorted = sorted(list(splits))
+        wc = ""
+        prev = 0
+        for i in splits_sorted:
+            if i == 0:
+                continue
+            wc += w[prev:i] + "-"
+            prev = i
+        wc += w[prev:]
+        if wc.endswith("-"):
+            wc = wc[:-1]
+        return wc
+
+    def _add_compound(self, comp: Dict[str, str], w: str, ws: str) -> None:
+        if ws is not None:
+            ws_merged = self._append_suffix_and_prefix(ws)
+            comp[w] = ws_merged
+            logging.debug(f"Result: {w}\t{ws}\t{ws_merged}")
+
+    def _process_compound(self, comp: Dict[str, str], w: str, wns: str) -> None:
+        wns_split = wns.split(" ")
+        if "-" in w and self.dash_words == self.DashBehaviour.REMOVE:
+            return
+        if self.dash_words == self.DashBehaviour.SPLIT:
+            ws = w.split("-")
+            for wi in ws:
+                res = self._generate_compound(wi, wns_split)
+                if res is not None:
+                    self._add_compound(comp, wi, res)
+            return
+        res = self._generate_compound(w, wns_split)
+        if res is not None:
+            self._add_compound(comp, w, res)
+
+    def _unknown_word_compounding(self, w: str) -> Tuple[str, Set[str]]:
+        cands = set()
+        for s in self.single_words:
+            if s.lower() in w.lower() and not s.lower() == w.lower():
+                cands.add(s)
+        cands_new = set()
+        for ci in cands:
+            if not contained_in(ci, cands):
+                cands_new.add(ci)
+        res = self._generate_compound(w, cands_new)
+        logging.debug(f"unknown1: {res}")
+        if res is None:
+            res = w
+        else:
+            res = self._append_suffix_and_prefix(res)
+        logging.debug(f"unknown2: {res}")
+        return (res, cands_new)
+
+    def _get_highest_prob(self, compounds: Iterable[str]) -> Tuple[int, float]:
+        probs = []
+        for c in compounds:
+            p = self._get_word_counts(c)
+            probs.append(p)
+        return get_max_idx(probs)
+
+    def read_word_count(self, name: str) -> None:
+        """
+        Read the word counts from a file formatted in two tab-separated columns:
+        the words in the first column, their count in the second.
+
+        The file can be opened with gzip if it ends in '.gz'.
+        """
+        for l in nopen(name):
+            ls = l.strip().split("\t")
+            if len(ls) < 2:
+                continue  # Don't crash on error-prone split
+            wc = int(ls[1])
+            self.word_count[ls[0]] = wc
+            self.total_word_count += wc
+
+    def read_knowledge(self, name: str) -> None:
+        """
+        Read a list of words and its splitting candidates generated from a
+        distributional thesaurus in a tab separated columns, the words in the first
+        column, their splitting candidates in the following ones.
+
+        The file can be opened with gzip if it ends in '.gz'.
+        """
+        for l in nopen(name):
+            ls = l.rstrip("\n").split("\t")
+            w = ls[0]
+            if not self._remove_word(w):
+                self._process_compound(self.comp1, w, ls[1])
+                self._process_compound(self.comp2, w, ls[2])
+                self._process_compound(self.comp3, w, ls[3])
+
+    def extract_single_words(self) -> None:
+        """
+        Extract single words from the first set of candidate splits extracted from the
+        knowledge file.
+        """
+        for c in self.comp1:
+            if "-" in self.comp1[c]:
+                self.single_words |= set(self.comp1[c].split("-"))
+
+    def prepare_decompounding(self, file_count: str, file_knowledge: str) -> None:
+        """
+        Calls read_word_count(file_count), read_knowledge(file_knowledge), and
+        extract_single_words in that order to prepare for compound splitting.
+        """
+        logging.info("reading word count")
+        self.read_word_count(file_count)
+        logging.info("reading knowledge")
+        self.read_knowledge(file_knowledge)
+        logging.info("extracting single words")
+        self.extract_single_words()
+
+
+    @staticmethod
+    def _get_first_dash(compounds: Iterable[str]) -> int:
+        i = 0
+        for c in compounds:
+            if "-" in c:
+                return i
+            i += 1
+        return -1
--- decompound_text_secos.py
+++ decompound_text_secos.py
@@ -2,11 +2,14 @@
 
 # Decompounds from stdin instead of reading a file directly
 
-import gzip
+import logging
 import sys
-from typing import IO, Dict, Iterable, List, Optional, Set, Tuple
 
-i = 0
+from decompound import Decompounder
+
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+)
 
 
 def eprint(*args, **kwargs) -> None:
@@ -53,277 +56,43 @@ if len(sys.argv) < 9:
     )
     eprint("epsilon:\t\tsmoothing factor (recommended parameter: 0.01")
     sys.exit(1)
+
+
+decompounder = Decompounder(
+    min_word_count=int(sys.argv[3]),
+    prefix_length=int(sys.argv[4]),
+    suffix_length=int(sys.argv[5]),
+    min_word_length=int(sys.argv[6]),
+    # 1 -> remove, 2 -> split, 3 -> nothing
+    dash_words=Decompounder.DashBehaviour(int(sys.argv[7])),
+    uppercase_first_letter=True if sys.argv[8] == "upper" else False,
+    epsilon=float(sys.argv[9]),
+)
+
+
 file_knowledge = sys.argv[1]
 file_wordcount = sys.argv[2]
-min_word_count = int(sys.argv[3])
 
-
-prefix_length = int(sys.argv[4])
-suffix_length = int(sys.argv[5])
-min_word_length = int(sys.argv[6])
-epsilon = float(sys.argv[9])
-# 1 -> remove, 2 -> split, 3 -> nothing
-dash_words = int(sys.argv[7])
-uppercaseFirstLetter = False
-if sys.argv[8] == "upper":
-    uppercaseFirstLetter = True
-
-
-debug = not True
-
-
-def nopen(f: str) -> IO[str]:
-    if f.endswith(".gz"):
-        return gzip.open(f)
-    return open(f, encoding="utf-8")
-
-
-words: Set[str] = set()
-total_word_count = 0
-word_count: Dict[str, int] = {}
-for l in nopen(file_wordcount):
-    ls = l.strip().split("\t")
-    if len(ls) < 2:
-        continue  # Differences between python2 and python3 strip method made it crash
-    wc = int(ls[1])
-    word_count[ls[0]] = wc
-    total_word_count += wc
-
-
-def removeWord(w: str) -> bool:
-    if len(w.replace("-", "")) == 0:
-        return True
-    if min_word_count <= 0:
-        return False
-    if w in word_count:
-        if word_count[w] >= min_word_count:
-            return False
-    return True
-
-
-def removeShortAndEqual(wc: str, ws: Iterable[str]) -> List[str]:
-    nws = set()
-    for w in ws:
-        if (
-            len(w) >= min_word_length
-            and w.lower() != wc.lower()
-            and not w.isupper()
-            and w.lower() in wc.lower()
-        ):
-            nws.add(w)
-    return list(nws)
-
-
-# count suffixes and prefixes
-fillers: Dict[str, int] = {}
-
-
-def addUp(w: str) -> None:
-    if w in fillers:
-        fillers[w] += 1
-    else:
-        fillers[w] = 1
-
-
-def appendSuffix(w: str) -> str:
-    nl = ""
-    # first append on the left side
-    for l in w.split("-"):
-        if len(l) > suffix_length:
-            nl += "-"
-        else:
-            addUp(l)
-        nl += l
-    nl = nl.strip("-")
-    return nl
-
-
-def appendPrefix(w: str) -> str:
-    # append to the right
-    nw = w
-    nl = ""
-    for l in nw.split("-"):
-        nl += l
-        if len(l) > prefix_length:
-            nl += "-"
-    if nl.endswith("-"):
-        nl = nl[:-1]
-    return nl
-
-
-def getWordCounts(comp: str) -> float:
-    sum = 1.0
-    for c in comp.split("-"):
-        if uppercaseFirstLetter:
-            c = c[0].upper() + c[1:]
-        if c in word_count:
-            sum *= (word_count[c] + epsilon) / (
-                total_word_count + epsilon * len(word_count)
-            )
-        else:
-            sum *= epsilon / (total_word_count + epsilon * len(word_count))
-    return pow(1.0 * sum, 1.0 / len(comp.split("-")))
-
-
-def appendSuffixAndPrefix(w: str) -> str:
-    sp = appendSuffix(appendPrefix(w))
-    ps = appendPrefix(appendSuffix(w))
-    spc = getWordCounts(sp)
-    psc = getWordCounts(ps)
-    if spc > psc:
-        return sp
-    return ps
-
-
-def generateCompound(w: str, ws: Iterable[str]) -> Optional[str]:
-    # remove too short words
-    nws = removeShortAndEqual(w, ws)
-    if len(nws) == 0:
-        if debug:
-            eprint(f"NONE: {w}")
-        return None
-    nws_sorted = sorted(nws, key=lambda x: len(x), reverse=True)
-    # get split points
-    splits = set()
-    for n in nws_sorted:
-        if not n.lower() in w.lower():
-            continue
-        idx = w.lower().index(n.lower())
-        splits.add(idx)
-        splits.add(idx + len(n))
-    splits_sorted = sorted(list(splits))
-    wc = ""
-    prev = 0
-    for i in splits_sorted:
-        if i == 0:
-            continue
-        wc += w[prev:i] + "-"
-        prev = i
-    wc += w[prev:]
-    if wc.endswith("-"):
-        wc = wc[:-1]
-    return wc
-
-
-def addCompound(comp: Dict[str, str], w: str, ws: str) -> None:
-    if ws is not None:
-        ws_merged = appendSuffixAndPrefix(ws)
-        comp[w] = ws_merged
-        if debug:
-            eprint(f"Result: {w}\t{ws}\t{ws_merged}")
-
-
-def processCompound(comp: Dict[str, str], w: str, wns: str) -> None:
-    wns_split = wns.split(" ")
-    if "-" in w and dash_words == 1:
-        return
-    if dash_words == 2:
-        ws = w.split("-")
-        for wi in ws:
-            res = generateCompound(wi, wns_split)
-            if res is not None:
-                addCompound(comp, wi, res)
-        return
-    res = generateCompound(w, wns_split)
-    if res is not None:
-        addCompound(comp, w, res)
-
-
-comp1: Dict[str, str] = {}
-comp2: Dict[str, str] = {}
-comp3: Dict[str, str] = {}
-eprint("read knowledge")
-
-
-for l in nopen(file_knowledge):
-    ls = l.rstrip("\n").split("\t")
-    w = ls[0]
-    if not removeWord(w):
-        processCompound(comp1, w, ls[1])
-        processCompound(comp2, w, ls[2])
-        processCompound(comp3, w, ls[3])
-eprint("extract single words")
-singlewords: Set[str] = set()
-for c in comp1:
-    if "-" in comp1[c]:
-        singlewords |= set(comp1[c].split("-"))
-eprint("decompound")
-
-
-def containedIn(c: str, cands: Iterable[str]) -> bool:
-    for cj in cands:
-        if c.lower() in cj.lower() and c.lower() != cj.lower():
-            return True
-    return False
-
-
-def unknownWordCompounding(w: str) -> Tuple[str, Set[str]]:
-    cands = set()
-    for s in singlewords:
-        if s.lower() in w.lower() and not s.lower() == w.lower():
-            cands.add(s)
-    cands_new = set()
-    for ci in cands:
-        if not containedIn(ci, cands):
-            cands_new.add(ci)
-    res = generateCompound(w, cands_new)
-    if debug:
-        eprint(f"unknown1: {res}")
-    if res is None:
-        res = w
-    else:
-        res = appendSuffixAndPrefix(res)
-    if debug:
-        eprint(f"unknown2: {res}")
-    return (res, cands_new)
-
-
-def getFirstDash(compounds: Iterable[str]) -> int:
-    i = 0
-    for c in compounds:
-        if "-" in c:
-            return i
-        i += 1
-    return -1
-
-
-def getMaxIdx(ls: Iterable[float]) -> Tuple[int, float]:
-    idx = -1
-    val = 0.0
-    i = 0
-    for l in ls:
-        if l > val:
-            val = l
-            idx = i
-        i += 1
-    return (idx, val)
-
-
-def getHighestProb(compounds: Iterable[str]) -> Tuple[int, float]:
-    probs = []
-    for c in compounds:
-        p = getWordCounts(c)
-        probs.append(p)
-    return getMaxIdx(probs)
-
+decompounder.prepare_decompounding(file_wordcount, file_knowledge)
 
 # NOTE: Different from decompound_secos.py
+# FIXME: gotta do this
 for l in sys.stdin:
     text = ""
     for w in l.strip().split():
         wc = -1
-        if w in word_count:
-            wc = word_count[w]
-        c1 = comp1.get(w, w)
-        c2 = comp2.get(w, w)
-        c3 = comp3.get(w, w)
-        [u, ufeats] = unknownWordCompounding(w)
+        if w in decompounder.word_count:
+            wc = decompounder.word_count[w]
+        c1 = decompounder.comp1.get(w, w)
+        c2 = decompounder.comp2.get(w, w)
+        c3 = decompounder.comp3.get(w, w)
+        [u, ufeats] = decompounder._unknown_word_compounding(w)
         cand = w
         cands = [c1, c2, c3, u]
-        idx = getFirstDash(cands)
+        idx = decompounder._get_first_dash(cands)
         if idx >= 0:
             cand = cands[idx]
-        [idx, prob] = getHighestProb(cands)
+        [idx, prob] = decompounder._get_highest_prob(cands)
         pcand = w
         if idx >= 0:
             pcand = cands[idx]
